2e864869dd6cd6cb8fb9b1d9e50c9a0cf16f6e84
Jan André Greschner <a01634939@unet.univie.ac.at>
2020-01-15 23:40:55 +0100

    test bot

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
-echo "HI"
+require_once 'vendor/autoload.php';
+
+$botKey = '1007196355:AAHFvhwo89yJZympLncHzrMPgityuwIJrg4';
+
+$requestFactory = new Http\Factory\Guzzle\RequestFactory();
+$streamFactory = new Http\Factory\Guzzle\StreamFactory();
+$client = new Http\Adapter\Guzzle6\Client();
+
+$apiClient = new \TgBotApi\BotApiBase\ApiClient($requestFactory, $streamFactory, $client);
+$bot = new \TgBotApi\BotApiBase\BotApi($botKey, $apiClient, new \TgBotApi\BotApiBase\BotApiNormalizer());
+
+$userId = 'SICosyBot';
+
+try {
+    $bot->send(\TgBotApi\BotApiBase\Method\SendMessageMethod::create($userId, 'Hi'));
+} catch (\TgBotApi\BotApiBase\Exception\BadArgumentException $e) {
+} catch (\TgBotApi\BotApiBase\Exception\ResponseException $e) {
+}
+
 
 ?>
