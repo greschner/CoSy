9da0d2bf1e0c17c2d5d2a207381d8841e72e437d
Jan André Greschner <a01634939@unet.univie.ac.at>
2020-01-22 23:20:54 +0100

    test InlineKeyboardButton.php

--- index.php
+++ index.php
@@ -17,24 +17,47 @@ $update = Update::create($data);
 
 $bot = new Bot('1007196355:AAHFvhwo89yJZympLncHzrMPgityuwIJrg4');
 
-if ($callbackQuery = $update->getCallbackQuery()) {
-    file_put_contents("query.txt",$callbackQuery->getMessage()->getText());
-    $bot->answerCallbackQuery(new AnswerCallbackQuery($callbackQuery->getId()));
+if ($message = $update->getMessage()){
+
+    if ($message->getText()=="/start"){
+        $b1 = InlineKeyboardButton::withTextAsCallbackData('Eltern');
+        $b2 = InlineKeyboardButton::withTextAsCallbackData('Lehrende');
+        $b3 = InlineKeyboardButton::withTextAsCallbackData('Jugendarbeit');
+        $b4 = InlineKeyboardButton::withTextAsCallbackData('Jugendliche');
+        $b5 = InlineKeyboardButton::withTextAsCallbackData('Senioren');
+        $b5 = InlineKeyboardButton::withTextAsCallbackData('Richard Lugner');
+        $keyboard = new InlineKeyboardMarkup([[$b1], [$b2], [$b3], [$b4], [$b5]]);
+
+        $sendMessage = new SendMessage($update->getMessage()->getChat()->getId(), 'Hallo! Ich bin deine Mama. Ich kenne mich sehr gut aus mit Fragen zum richtigen Umgang mit dem Internet. Damit ich dir besser helfen kann wähle bitte die Zielgruppe, der du dich am ehesten zugehörig fühlst:');
+        $sendMessage->setReplyMarkup($keyboard);
+        $bot->sendMessage($sendMessage);
+    }
 }
 
-if ($update->getMessage()->getText()=="/start"){
+if ($callbackQuery = $update->getCallbackQuery()) {
 
-    $b1 = InlineKeyboardButton::withTextAsCallbackData('Eltern');
-    $b2 = InlineKeyboardButton::withTextAsCallbackData('Lehrende');
-    $b3 = InlineKeyboardButton::withTextAsCallbackData('Jugendarbeit');
-    $b4 = InlineKeyboardButton::withTextAsCallbackData('Jugendliche');
-    $b5 = InlineKeyboardButton::withTextAsCallbackData('Senioren');
-    $b5 = InlineKeyboardButton::withTextAsCallbackData('Richard Lugner');
-    $keyboard = new InlineKeyboardMarkup([[$b1], [$b2], [$b3], [$b4], [$b5]]);
+    switch ($callbackQuery->getData()){
+        case "Eltern":
+            $answerCallbackQuery = new AnswerCallbackQuery($callbackQuery->getId());
+            $answerCallbackQuery->setText("Toll. Das hat so weit gut funktioniert. Wenn du deine Auswahl später ändern willst schicke mir einfach eine neue Nachricht mit /start");
 
-    $sendMessage = new SendMessage($update->getMessage()->getChat()->getId(), 'Hallo! Ich bin deine Mama. Ich kenne mich sehr gut aus mit Fragen zum richtigen Umgang mit dem Internet. Damit ich dir besser helfen kann wähle bitte die Zielgruppe, der du dich am ehesten zugehörig fühlst:');
-    $sendMessage->setReplyMarkup($keyboard);
-    $bot->sendMessage($sendMessage);
+            $b1 = InlineKeyboardButton::withTextAsCallbackData('Frage stellen');
+            $b2 = InlineKeyboardButton::withTextAsCallbackData('Themen anzeigen');
+            $b3 = InlineKeyboardButton::withTextAsCallbackData('Imm Moment nichts');
+
+            $keyboard = new InlineKeyboardMarkup([[$b1], [$b2], [$b3]]);
+
+            $sendMessage = new SendMessage($callbackQuery->getMessage()->getChat()->getId(), 'Ich kann dir anbieten eine Frage zu beantworten, oder dir verschiedene Themen vorschlagen. Was klingt besser?');
+            $sendMessage->setReplyMarkup($keyboard);
+            $bot->answerCallbackQuery($answerCallbackQuery);
+            $bot->sendMessage($sendMessage);
+            break;
+        default:
+            $bot->answerCallbackQuery(new AnswerCallbackQuery($callbackQuery->getId()));
+    }
+
+    file_put_contents("query.txt",$callbackQuery->getMessage()->getText());
+
 }
 
 /*if ($callbackQuery = $update->getCallbackQuery()) {
