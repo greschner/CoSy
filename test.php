9ebf3c53e60e305d031e296a81b8280d6bf86dd3
Mathais Hoeller <a01555666@unet.univie.ac.at>
2020-01-27 13:29:25 +0100

    test searchphp

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+$opts = array('http' =>
+	    array(
+		'method'  => 'GET',
+		'header'  => 'Content-type: application/json',
+	    )
+	);
+	$context = stream_context_create($opts);
+	$result = file_get_contents('search.php?search_term=facebook&role=jugendliche', false, $context);
+	echo $result;
+?>
