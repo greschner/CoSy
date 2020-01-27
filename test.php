1f9973aa26be02a0e38e26cfc117ddca9d8a743b
Mathais Hoeller <a01555666@unet.univie.ac.at>
2020-01-27 13:37:50 +0100

    test.php

--- test.php
+++ test.php
@@ -1,12 +1,28 @@
-<?php
+ <!DOCTYPE html>
+<html>
+<body>
 
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo 'test';
+exit;
 $opts = array('http' =>
 	    array(
 		'method'  => 'GET',
-		'header'  => 'Content-type: application/json',
+		'header'  => 'Content-type: application/json'
 	    )
 	);
 	$context = stream_context_create($opts);
 	$result = file_get_contents('search.php?search_term=facebook&role=jugendliche', false, $context);
 	echo $result;
+	echo 'test';
 ?>
+
+</body>
+</html> 
+
